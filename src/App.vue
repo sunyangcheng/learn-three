334b86f7685875838e9a3191f5f8721d04e30d79
sunyoung <cn.sunyoung@gmail.com>
2022-08-16 22:56:23 +0800

    add basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">
+    <div class="canvas-container" ref="canvasDom"></div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<script setup>
+import * as THREE from 'three'; 
+import { onMounted, reactive, ref } from 'vue';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+let controls;
+let canvasDom = ref(null);
+// 创建场景
+const scene = new THREE.Scene();
+// 创建相机
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 2, 6);
+// 创建渲染器
+const renderer = new THREE.WebGLRenderer({
+  // 抗锯齿
+  antialias: true 
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const render = () => {
+  renderer.render(scene, camera);
+  controls && controls.update();
+  requestAnimationFrame(render);
 }
+
+onMounted(() => {
+  // 将渲染器添加到页面中
+  canvasDom.value.appendChild(renderer.domElement);
+  // 初始化渲染器，渲染背景
+  renderer.setClearColor("#000");
+  scene.background = new THREE.Color("#ccc");
+  scene.environment = new THREE.Color("#ccc");
+  render();
+
+  // 添加网格地面
+  const gridHelper = new THREE.GridHelper(10, 10);
+  gridHelper.material.opacity = 0.2;
+  gridHelper.material.transparent = true;
+  scene.add(gridHelper);
+
+  // 添加控制器
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.update();
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
